779cb54062e323d93a8bd818c14d9b466852956c
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-03 03:45:43 -0500

    Added instance variables for Player class

--- src/server/Player.java
+++ src/server/Player.java
@@ -1,4 +1,17 @@
 package server;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    String playerColor;
+    int foodCards;
+    int animalCards;
+    int shelterCards;
+    int habitatsEarned;
+    int keeperCards;
+    ArrayList<String> selections;
+    boolean luckyDay;
+    int playerID;
+
 }
