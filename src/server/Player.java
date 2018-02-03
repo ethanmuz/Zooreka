2e4a90d1cb2464cfc69e4be7faf708731ac033f4
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-03 06:13:11 -0500

    Added rest of getters

--- src/server/Player.java
+++ src/server/Player.java
@@ -20,19 +20,53 @@ public class Player {
      *
      * @param id Server tells this Player what its Player ID number is
      */
-    public Player(int id){
+    public Player(int id) {
         this.playerID = id; //Assign player ID to the ID passed in by server
         this.foodCards = 2; //The rules say each player starts with 2 food cards
         this.animalCards = 1; //The rules say each player starts with 1 animal card
         this.shelterCards = 0; //No player starts with shelter cards
         this.habitatsEarned = 0; //No player starts with habitats
         this.keeperCards = 0;  //No player starts with keeper cards
-        this.luckyDay = false; this.luckySelection = null;  //No player starts with Lucky Day
+        this.luckyDay = false;
+        this.luckySelection = null;  //No player starts with Lucky Day
 
     }
 
-    public int getID(){
+    public int getID() {
         return this.playerID; //Returns this Player's ID number
     }
+
+    public int getFood() {
+        return this.foodCards; //Returns number of food cards this Player has
+    }
+
+    public int getAnimalCards() {
+        return this.animalCards; //Returns number of animal cards this Player has
+    }
+
+    public int getShelterCards() {
+        return this.shelterCards; //Returns number of shelter cards this Player has
+    }
+
+    public int getHabitats() {
+        return this.habitatsEarned; //Returns number of habitats this Player has
+    }
+
+    public int getKeeperCards() {
+        return this.keeperCards; //Returns number of keeper cards this Player has
+    }
+
+    public String getSelection() {
+        return this.selection; //Returns the resource this Player has selected (as a String "food", "animal", or "shelter")
+    }
+
+    public boolean isLuckyDay() {
+        return this.luckyDay; //Returns whether or not it is this Player's lucky day
+    }
+
+    public String getLuckySelection() {
+        return this.luckySelection; //Returns the lucky resource this Player has selected (if one; as a String "food", "animal", or "shelter")
+    }
+
 
 }
