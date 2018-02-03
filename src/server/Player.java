e3becfa2918057ab148b0ee0743096d761c67d34
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-03 04:57:56 -0500

    Added comments to Player fields

--- src/server/Player.java
+++ src/server/Player.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 
 public class Player {
 
-    String playerColor;
-    int foodCards;
-    int animalCards;
-    int shelterCards;
-    int habitatsEarned;
-    int keeperCards;
-    ArrayList<String> selections;
-    boolean luckyDay;
-    int playerID;
+    //String playerColor; //???
+    int foodCards; //Number of food cards this player possesses
+    int animalCards; //Number of animal cards this player possesses
+    int shelterCards; //Number of shelter cards this player has
+    int habitatsEarned; //Number of habitats this player has earned so far
+    int keeperCards; //Number of keeper cards this player possesses
+    String selection; //Initial resource selection
+    String luckySelection; //Resource selected with lucky token
+    boolean luckyDay; //Whether or not it is this player's "lucky day", that is whether or not they possess the lucky token.
+    int playerID; //Player number used for ID purposes
 
 }
