fb381d4e070f68227376d34b64289793bc1b519c
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-03 05:22:29 -0500

    Built the constructor for Player. Also made all instance variables private

--- src/server/Player.java
+++ src/server/Player.java
@@ -5,14 +5,26 @@ import java.util.ArrayList;
 public class Player {
 
     //String playerColor; //???
-    int foodCards; //Number of food cards this player possesses
-    int animalCards; //Number of animal cards this player possesses
-    int shelterCards; //Number of shelter cards this player has
-    int habitatsEarned; //Number of habitats this player has earned so far
-    int keeperCards; //Number of keeper cards this player possesses
-    String selection; //Initial resource selection
-    String luckySelection; //Resource selected with lucky token
-    boolean luckyDay; //Whether or not it is this player's "lucky day", that is whether or not they possess the lucky token.
-    int playerID; //Player number used for ID purposes
+    private int foodCards; //Number of food cards this player possesses
+    private int animalCards; //Number of animal cards this player possesses
+    private int shelterCards; //Number of shelter cards this player has
+    private int habitatsEarned; //Number of habitats this player has earned so far
+    private int keeperCards; //Number of keeper cards this player possesses
+    private String selection; //Initial resource selection
+    private String luckySelection; //Resource selected with lucky token
+    private boolean luckyDay; //Whether or not it is this player's "lucky day", that is whether or not they possess the lucky token.
+    private int playerID; //Player number used for ID purposes
+
+
+    public Player(int id){
+        this.playerID = id; //Assign player ID to the ID passed in by server
+        this.foodCards = 2; //The rules say each player starts with 2 food cards
+        this.animalCards = 1; //The rules say each player starts with 1 animal card
+        this.shelterCards = 0; //No player starts with shelter cards
+        this.habitatsEarned = 0; //No player starts with habitats
+        this.keeperCards = 0;  //No player starts with keeper cards
+        this.luckyDay = false; this.luckySelection = null;  //No player starts with Lucky Day
+
+    }
 
 }
