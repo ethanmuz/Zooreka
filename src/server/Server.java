34c8f0b3d5406d74ca595e2f13822578bdba13f1
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-13 22:33:11 -0500

    Added Blue Skies method to Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,39 @@
 package server;
 
+import java.util.*;
+
 public class Server {
 
+    public void BlueSkies(Player player){
+        Random random = new Random(); int card = random.nextInt(9);
+        switch (card) {
+            case 0:
+                player.addKeeperCard(); //Player draws a 'keeper' Blue Skies card
+                break;
+            case 1:
+                player.changeResources(0, 0, 1, 0); //Player's Blue Skies card gives them one shelter card
+                break;
+            case 2:
+                player.changeResources(2, 2, 0, 0); //Player's Blue Skies card gives them two animal cards and two food cards
+                break;
+            case 3:
+                player.changeResources(1, 2, 0, 0); //Player's Blue Skies card gives them two animal cards and one food card
+                break;
+            case 4:
+                player.changeResources(0, 2, 0, 0); //Player's Blue Skies card gives them two animal cards
+                break;
+            case 5:
+                player.changeResources(3, 1, 0, 0); //Player's Blue Skies card gives them one animal card and three food cards
+                break;
+            case 6:
+                player.changeResources(2, 1, 0, 0); //Player's Blue Skies card gives them one animal card and two food cards
+                break;
+            case 7:
+                player.changeResources(1, 1, 0, 0); //Player's Blue Skies card gives them one animal card and one food card
+                break;
+            case 8:
+                player.changeResources(3, 0, 0, 0); //Player's Blue Skies card gives them three food cards
+                break;
+        }
+    }
 }
