288b94bb0063ede9a7982d7f17dbf80b48d5ad33
Ethan Uzarowski <ethanmuz@gmail.com>
2018-02-13 22:54:01 -0500

    Added Stormy Weather method to Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -36,4 +36,31 @@ public class Server {
                 break;
         }
     }
+
+    public void StormyWeather(Player player){
+        Random random = new Random(); int card = random.nextInt(7);
+        switch (card) {
+            case 0:
+                player.changeResources(-1, -2, 0, 0); //Player's Stormy Weather card takes away two animal cards and one food card
+                break;
+            case 1:
+                player.changeResources(0, -2, 0, 0); //Player's Stormy Weather card takes away two animal cards
+                break;
+            case 2:
+                player.changeResources(-2, -1, 0, 0); //Player's Stormy Weather card takes away one animal card and two food cards
+                break;
+            case 3:
+                player.changeResources(-1, -1, 0, 0); //Player's Stormy Weather card takes away one animal card and one food card
+                break;
+            case 4:
+                player.changeResources(-3, 0, 0, 0); //Player's Stormy Weather card takes away three food cards
+                break;
+            case 5:
+                player.changeResources(-2, 0, 0, 0); //Player's Stormy Weather card takes away two food cards
+                break;
+            case 6:
+                player.changeResources(-1, 0, 0, 0); //Player's Stormy Weather card takes away one food card
+                break;
+        }
+    }
 }
